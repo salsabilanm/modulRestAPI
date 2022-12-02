26db0c718c2730dd78ce9aa4f1a29de52a86fa93
Salsabila Nur Malahayati <salsabilanm8@gmail.com>
2022-12-02 18:27:05 +0700

    modul Rest API

---
+++ app/src/main/java/API/ApiService.java
@@ -0,0 +1,23 @@
+package API;
+
+import Model.AddMahasiswaResponse;
+import Model.MahasiswaResponse;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    @GET("mahasiswa")
+    Call<MahasiswaResponse> getMahasiswa(@Query("nrp") String nrp);
+    @POST("mahasiswa")
+    @FormUrlEncoded
+    Call<AddMahasiswaResponse> addMahasiswa(
+            @Field("nrp") String nrp,
+            @Field("nama") String nama,
+            @Field("email") String email,
+            @Field("jurusan") String jurusan
+    );
+}
---
+++ app/src/main/java/Model/MahasiswaResponse.java
@@ -0,0 +1,20 @@
+package Model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MahasiswaResponse {
+    @SerializedName("data")
+    private List<Mahasiswa> data;
+    @SerializedName("status")
+    private boolean status;
+
+    public List<Mahasiswa> getData() {
+        return data;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+}
